2e5c546718274d89cedc1e1ccba40e64ff9522d2
sarah-williams1 <111523026+sarah-williams1@users.noreply.github.com>
2024-05-09 11:57:10 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Assignment_1._4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point1 = new Point();
+            Point point2 = new Point();
+ // Point is a custom data type that I defined in the method below.
+ // point1 is the variable name
+ // point2 is the other variable name
+ // if else statement compares the two values and prints a text depending on the result.
+            if (point1.x == point2.x)
+            {
+                Console.WriteLine("Points are on the same axis.");
+            }
+            else
+            {
+                Console.WriteLine("Point is not on the same axis");
+            }
+
+        }
+
+    public struct Point 
+    {
+ // struct Point is the method
+ // Point is also a custom data type
+
+       public double x;
+       public double y;
+
+
+     }        
+
+
+        
+    }
+}
