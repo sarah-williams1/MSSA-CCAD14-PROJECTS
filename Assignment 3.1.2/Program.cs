6376bb31514c598db13036ef551c1f8c266557a7
sarah-williams1 <111523026+sarah-williams1@users.noreply.github.com>
2024-05-20 16:41:45 -0400

    Add files via upload
    Assignment 3.1.2

---
+++ Assignment 3.1.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Assignment_3._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a year. This awesome program will check if it is a leap year or not a leap year.\n");
+            string enteredYear = Console.ReadLine();
+
+            int.TryParse(enteredYear, out int integerYear);
+             if (DateTime.IsLeapYear(integerYear))
+            {
+                Console.WriteLine(integerYear + " is a leap year!\n\tDon't forget to leap for leap year!");
+            }
+             else
+            {
+                Console.WriteLine(integerYear + " is not a leap year\n\twomp womp");
+            }
+        }       
+    }
+}
