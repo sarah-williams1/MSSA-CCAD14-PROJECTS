5bb679317b4d833654c8edf9130768ec4e94a5a5
sarah-williams1 <111523026+sarah-williams1@users.noreply.github.com>
2024-05-20 14:36:52 -0400

    Add files via upload
    Assignment 3.1.1

---
+++ Assignment 3.1.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+
+namespace Assignment_3._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int integerEvenNumber = 0;
+
+            for (integerEvenNumber = 0; integerEvenNumber <= 101; integerEvenNumber += 2)
+            {
+                Console.WriteLine(" " + integerEvenNumber);
+            }
+            */
+
+            
+            StringBuilder evenNumber = new StringBuilder();
+            for (int integerEvenNumber = 0; integerEvenNumber <= 101; integerEvenNumber += 2)
+            {
+                Console.WriteLine(integerEvenNumber.ToString());
+            }
+           
+            Console.WriteLine(evenNumber);
+        }
+    }
+}
