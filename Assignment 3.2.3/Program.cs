e4a9949c228f9f2eaab7661e36e0cc031e26a1df
sarah-williams1 <111523026+sarah-williams1@users.noreply.github.com>
2024-05-21 16:29:56 -0400

    Add files via upload

---
+++ Assignment 3.2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment_3._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! This program will take the areas of two circles and add them together or find the difference between the two.");
+            Console.WriteLine();
+            Console.WriteLine("Pleae input the radius of Circle A");
+            double radiusCircleA =  Console.ReadLine();
+            Console.WriteLine("\n");
+            Console.WriteLine("Please input the radius of Circle B.");
+            Console.ReadLine();
+        }
+    }
+}
