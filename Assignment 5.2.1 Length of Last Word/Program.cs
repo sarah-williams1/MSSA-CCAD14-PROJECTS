fe166b5e351f90b3e9258e7875c72b1751831393
sarah-williams1 <111523026+sarah-williams1@users.noreply.github.com>
2024-06-04 14:42:13 -0400

    Add files via upload
    Assignment 5.2.1

---
+++ Assignment 5.2.1 Length of Last Word/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Assignment_5._2._1_Length_of_Last_Word
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Given a string consisting of words and spaces, return the length of the last word in the string.");
+          
+            string inputString ="Fly me to the moon";
+            Console.WriteLine(inputString);
+            string[] inputStringArray = inputString.TrimEnd().Split(' ');
+            
+            // int lastWordLength takes the length of the input string and subtracts 1.
+            // This is because strings are arrays, and indexes start at 0.
+            string lastWord = inputStringArray[inputStringArray.Length - 1];
+            int lastWordLength = lastWord.Length;
+            Console.WriteLine($"The length of the last word is: {lastWordLength}");          
+        }
+    }
+}
