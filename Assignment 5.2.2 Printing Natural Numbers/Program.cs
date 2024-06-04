1ac9b3e551badcf69daf996e6940f954da14cf6c
sarah-williams1 <111523026+sarah-williams1@users.noreply.github.com>
2024-06-04 15:20:41 -0400

    Add files via upload
    Assignment 5.2.2 Printing Natural Numbers

---
+++ Assignment 5.2.2 Printing Natural Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Assignment_5._2._2_Printing_Natural_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program will print out the first n number of natural integers through the power of recurssion.");
+            Console.WriteLine("Please type the number of numbers you'd like to see here: ");
+            string numberToPrint = Console.ReadLine();
+            int numberToReturn = Convert.ToInt32(numberToPrint);
+            NaturalNumber(numberToReturn);
+        }
+
+        static void NaturalNumber(int numberToReturn)
+        {
+            if (numberToReturn < 1)
+            {
+                return;
+            }
+            NaturalNumber(numberToReturn - 1);
+            Console.Write(" " + numberToReturn);
+        }
+    }
+}
