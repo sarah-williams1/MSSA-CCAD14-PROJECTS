816e59d9794b648d200749f0308bcd027cf07996
sarah-williams1 <111523026+sarah-williams1@users.noreply.github.com>
2024-06-04 15:29:15 -0400

    Add files via upload

---
+++ Assignment 5.2.3 Printing Natural Numbers In Reverse/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Assignment_5._2._2_Printing_Natural_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program will print out the first n number of natural integers through the power of recurssion. The numbers will be printed in reverse!");
+            Console.WriteLine("Please type the number of numbers you'd like to see here: ");
+            string numberToPrint = Console.ReadLine();
+            Console.WriteLine();
+            int numberToReturn = Convert.ToInt32(numberToPrint);
+            NaturalNumber(numberToReturn);
+        }
+
+        static void NaturalNumber(int numberToReturn)
+        {
+            if (numberToReturn < 1)
+            {
+                return;
+            }
+            Console.Write(numberToReturn+" ");
+            NaturalNumber(numberToReturn - 1);
+        }
+    }
+}
