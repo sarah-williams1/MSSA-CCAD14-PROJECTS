d4e0b35a6f2a9ea63635902d5ade5805de12baac
sarah-williams1 <111523026+sarah-williams1@users.noreply.github.com>
2024-06-05 16:07:31 -0400

    Add files via upload

---
+++ Assignment 5.3.1 Flower Pots/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Assignment_5._3._1_Flower_Pots
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Flowers cannot be planted in adjacent pots. 0 means the flower pot is empty, and 1 means the flower pot is full.");
+            int[] flowerBedArray = { 1, 0, 0, 0, 1 };
+            int length = flowerBedArray.Length;
+            int flowerPots = FlowerPlacement(flowerBedArray);
+            Console.Write("The number of flowers you'd like to plant are: ");
+            Console.WriteLine(flowerPots);
+
+
+        }
+
+        public bool FlowerPlacement(int[] flowerBedArray, int length)
+        {
+            var count = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+
+                if (flowerBedArray[i] == 0 && (i == 0 || flowerBedArray[i - 1] == 0) && (i == length - 1 || flowerBedArray[i + 1] == 0))
+                {
+                    flowerBedArray[i++] = 1;
+                    count++;
+                }
+                if (count >= i)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+               
+            }
+            return count >= length;
+        }
+    }
+}
